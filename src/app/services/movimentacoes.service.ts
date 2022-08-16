f79aa6464a62d7ad9e741ea43fd8f7a98c37a5c0
unknown <thiagodistend@hotmail.com>
2022-08-16 18:11:06 -0300

    url heroku

--- src/app/services/movimentacoes.service.ts
+++ src/app/services/movimentacoes.service.ts
@@ -3,8 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 //mude para sua aplicação
-const baseUrl = 'http://localhost:8080';
-// const baseUrl = 'https://banklineapisantander.herokuapp.com';
+// const baseUrl = 'http://localhost:8080';
+const baseUrl = 'https://banklineapisantander.herokuapp.com';
 
 @Injectable({
   providedIn: 'root'
